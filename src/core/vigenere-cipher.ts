59296b1e968d664561240f6ab7d2c17c1946c8c8
Nick Babcock <nbabcock19@hotmail.com>
2019-07-27 18:48:02 -0500

    Prettier

--- src/core/vigenere-cipher.ts
+++ src/core/vigenere-cipher.ts
@@ -1,6 +1,5 @@
-const aCharCode = 'A'.charCodeAt(0);
-const zCharCode = 'Z'.charCodeAt(0);
-
+const aCharCode = "A".charCodeAt(0);
+const zCharCode = "Z".charCodeAt(0);
 
 // Zip two arrays together to form an array of two element tuples
 export function zip<T1, T2>(a: T1[], b: T2[]): [T1, T2][] {
@@ -57,7 +56,7 @@ export function decrypt(ciphertext: string, raw_key: string): string {
 // distributed uniformly in a sequential and round-robin fashion
 export function cosets(text: string, num: number): string[][] {
   const result: string[][] = new Array(num).fill(0).map(() => []);
-  const chars = text.split('');
+  const chars = text.split("");
   for (let i = 0; i < chars.length; i++) {
     result[i % num].push(chars[i]);
   }
@@ -69,26 +68,25 @@ export function cosets(text: string, num: number): string[][] {
 function frequencyCount(coset: string[]): number[] {
   const counts = new Array(26).fill(0);
   for (let letter of coset) {
-    counts[letter.charCodeAt(0) - aCharCode] += 1
+    counts[letter.charCodeAt(0) - aCharCode] += 1;
   }
   return counts;
 }
 
 // Index of coincidence for a coset
 export function coincidenceIndex(coset: string[]): number {
-  const fc = frequencyCount(coset); 
+  const fc = frequencyCount(coset);
   const sum = fc.map(x => x * (x - 1)).reduce((acc, x) => acc + x);
-  return sum / (coset.length * (coset.length - 1))
+  return sum / (coset.length * (coset.length - 1));
 }
 
 // Give an estimate of a potential key length of a cipher text. The algorithm
 // works by finding which coset length has the greatest index of coincidence.
 export function estimateKeyLength(cipherText: string, maxLen: number): number {
-  const indices = new Array(maxLen).fill(0)
-    .map((x, i) => {
-      const cis = cosets(cipherText, i + 1).map(x => coincidenceIndex(x));
-      return cis.reduce((a, b) => a + b, 0) / cis.length;
-    })
+  const indices = new Array(maxLen).fill(0).map((x, i) => {
+    const cis = cosets(cipherText, i + 1).map(x => coincidenceIndex(x));
+    return cis.reduce((a, b) => a + b, 0) / cis.length;
+  });
 
   return indices.indexOf(Math.max(...indices)) + 1;
 }
@@ -126,17 +124,16 @@ export function cosetShift(coset: string[]): number {
     0.00074
   ];
 
-  const chi = new Array(26)
-    .fill(0)
-    .map((_, i) => {
-        const shift = coset
-            .map(x => x.charCodeAt(0) - i)
-            .map(x => zCharCode - ((zCharCode - x) % 26))
-            .map(x => String.fromCharCode(x));
-        const fc = frequencyCount(shift);
-        return fc.map((x, i) => (((x / coset.length) - freq[i]) ** 2) / freq[i])
-          .reduce((acc, x) => acc + x);
-    });
+  const chi = new Array(26).fill(0).map((_, i) => {
+    const shift = coset
+      .map(x => x.charCodeAt(0) - i)
+      .map(x => zCharCode - ((zCharCode - x) % 26))
+      .map(x => String.fromCharCode(x));
+    const fc = frequencyCount(shift);
+    return fc
+      .map((x, i) => (x / coset.length - freq[i]) ** 2 / freq[i])
+      .reduce((acc, x) => acc + x);
+  });
 
   return chi.indexOf(Math.min(...chi));
 }
@@ -146,10 +143,16 @@ interface VigenereRecovery {
   plainText: string;
 }
 
-export function recoverVigenere(cipherText: string, maxKeyLen: number): VigenereRecovery {
+export function recoverVigenere(
+  cipherText: string,
+  maxKeyLen: number
+): VigenereRecovery {
   const keyLen = estimateKeyLength(cipherText, maxKeyLen);
   const coset = cosets(cipherText, keyLen);
-  const key = coset.map(x => cosetShift(x)).map(x => String.fromCharCode(aCharCode + x)).join('');
+  const key = coset
+    .map(x => cosetShift(x))
+    .map(x => String.fromCharCode(aCharCode + x))
+    .join("");
   const plainText = decrypt(cipherText, key);
   return { key, plainText };
 }
