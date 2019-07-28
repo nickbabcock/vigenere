ffc9c4ddb204616b64eb5462c1694f3dd640df0d
Nick Babcock <nbabcock19@hotmail.com>
2019-07-28 09:34:20 -0500

    Add poor man asset digest

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+npm run build
+npm run css
+
+# Poor man's asset pipeline
+asset_pipeline() {
+  FILE="$1"
+  FILE_EXT="${FILE##*.}"
+  FILE_NAME="${FILE%.*}"
+  SHA=$(sha1sum < "public/$FILE" | cut -d' ' -f1)
+  NEW_FILE="$FILE_NAME-$SHA.$FILE_EXT"
+  mv "public/$FILE" "public/$NEW_FILE"
+  sed -i -e "s/$FILE/$NEW_FILE/g" public/index.html
+}
+
+cp src/index.html public/index.html
+
+asset_pipeline bundle.js
+asset_pipeline styles.css
