30880dd3030c2bee586b5ad180be8eb69f7731b4
Nick Babcock <nbabcock19@hotmail.com>
2019-07-28 09:01:06 -0500

    Write svelte components

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,55 @@
 <script>
-	export let name;
+  import { encrypt, decrypt, recoverVigenere } from './core/vigenere-cipher';
+
+  let cryptModes = [
+    { title: 'Encryption', fn: encrypt, valueLabel: 'Cipher Key' },
+    { title: 'Decryption With Key', fn: decrypt, valueLabel: 'Cipher Key' },
+    { title: 'Decryption', fn: recoverVigenere, valueLabel: 'Max Key Length'}
+  ];
+
+  let selectedMode = cryptModes[0];
+  let valueText = '';
+  let cipherInput = '';
+  let cipherOutput = '';
+  $: if (valueText && cipherInput && (selectedMode != cryptModes[2] || !isNaN(valueText))) {
+    cipherOutput = selectedMode.fn(cipherInput, isNaN(valueText) ? valueText : +valueText);
+  }
+
+  function resetMode() {
+    cipherOutput = '';
+    cipherInput = '';
+    valueText = '';
+  }
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
+<table>
+  <tr>
+    <td>Cryptographic mode:</td>
+    <td>
+      <select bind:value={selectedMode} on:change={resetMode}>
+        {#each cryptModes as mode}
+          <option value={mode}>
+            {mode.title}
+          </option>
+        {/each}
+      </select>
+    </td>
+  </tr>
+  <tr>
+    <td>{selectedMode.valueLabel}</td>
+    <td>
+      <input bind:value={valueText}/>
+    </td>
+  </tr>
+</table>
 
-<h1>Hello {name}!</h1>
+<div>
+  <textarea placeholder="input text here" bind:value={cipherInput}/>
+</div>
+
+{#if selectedMode == cryptModes[2] && cipherOutput && cipherOutput.key}
+  <p>{`Recovered key: ${cipherOutput.key}`}</p>
+  <p>{cipherOutput.plainText}</p>
+{:else}
+  <p>{cipherOutput ? cipherOutput : ''}</p>
+{/if}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import App from './App.svelte';
-import { encrypt, decrypt, recoverVigenere } from './core/vigenere-cipher';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: recoverVigenere(decrypt(encrypt('hello world', 'abc'), 'abc'), 10).plainText
-	}
+	target: document.getElementById("app"),
 });
 
 export default app;
