88b591072ffcc40d5ad58748be07c2affa54e48b
Nick Babcock <nbabcock19@hotmail.com>
2024-09-07 10:20:57 -0500

    Update playwright to test over more uncommon port

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { PlaywrightTestConfig } from "@playwright/test";
 const config: PlaywrightTestConfig = {
   webServer: {
     command: "npm run build && npm run start",
-    port: 3000,
+    port: 3335,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
