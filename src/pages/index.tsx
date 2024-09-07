e24950734f477635465af8c040d76837f42a3810
Nick Babcock <nbabcock19@hotmail.com>
2024-09-07 10:43:39 -0500

    Update intro styling and include github icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { Cipher } from "../features/cipher";
+import Link from "next/link";
+import { GithubIcon } from "../components/GithubIcon";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col gap-y-4 p-3">
+    <div className="flex min-h-screen flex-col gap-y-4 px-3 pb-3 pt-8 sm:pt-12 md:pt-16 lg:pt-20 xl:pt-24">
       <Head>
         <title>Vigenère Cipher</title>
         <meta
@@ -16,29 +18,31 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="mx-auto max-w-prose space-y-4">
-        <h1 className="text-2xl font-bold">Vigenère Cipher</h1>
+        <div className="flex">
+          <h1 className="grow text-4xl font-extrabold">Vigenère Cipher</h1>
+          <Link href="https://github.com/nickbabcock/vigenere">
+            <GithubIcon
+              className="h-8 w-8 dark:fill-white"
+              alt="XPick Github Repo"
+            />
+          </Link>
+        </div>
         <p className="text-lg">
           The{" "}
           <a href="https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher">
             Vigenère Cipher
-          </a>{" "}
-          is a slightly more complex version of the{" "}
+          </a>
+          , encrypts text with an incrementing{" "}
           <a href="https://en.wikipedia.org/wiki/Caesar_cipher">
             Caesar Cipher
           </a>
-          , where instead of rotating plaintext by a single letter, Vigenère
-          allows rotation by words. Despite the increase in intricacy, both
-          Vigenère and Caesar ciphers are susceptible to frequency analysis. One
-          can recover the plaintext and the key given sufficiently long
-          ciphertext due to inherent patterns in english text.{" "}
+          . One can leverage frequency analysis to recover the plaintext and the
+          key given sufficiently long ciphertext due to inherent patterns in
+          english text.{" "}
           <a href="https://pages.mtu.edu/~shene/NSF-4/Tutorial/index.html">
             Dr. C.-K. Shene breaks down how this method works
           </a>
           .
-        </p>
-        <p className="text-lg">
-          Any code suggestions or issues can be{" "}
-          <a href="https://github.com/nickbabcock/vigenere">raised on Github</a>
         </p>
       </div>
       <Cipher />
